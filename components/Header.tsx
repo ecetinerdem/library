074472f0af8124fcca7e914b1ec7967c597a0188
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-12 01:36:41 +0400

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
@@ -8,7 +9,14 @@ const Header = () => {
   const pathname = usePathname();
   return (
     <header className="my-10 flex justify-between gap-5">
-      <Link href="/">BookWise</Link>
+      <Link href="/">
+        <Image
+          src="/icons/logo.svg"
+          alt="BookWise logo"
+          width={40}
+          height={40}
+        />
+      </Link>
       <ul className="flex flex-row item gap-8">
         <li>
           <Link
