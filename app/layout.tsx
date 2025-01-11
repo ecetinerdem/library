4aab55a0fc4372b961abe194a6028bcb8619e355
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-12 00:35:59 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from "react";
 import type { Metadata } from "next";
 import "./globals.css";
 import localFont from "next/font/local";
@@ -23,18 +24,16 @@ export const metadata: Metadata = {
   description: "Library management system",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${ibmPlexSans.className} ${bebasNeue.variable} antialiased`}
       >
         {children}
       </body>
     </html>
   );
-}
+};
+
+export default RootLayout;
