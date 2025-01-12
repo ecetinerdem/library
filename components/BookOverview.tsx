c148bc604e85e3c99a1c9dcaac5d6d56d6448453
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-12 20:32:07 +0400

    add Book interface

--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -10,11 +10,19 @@ const BookOverview = ({
   description,
   color,
   cover,
-}) => {
+}: Book) => {
   return (
     <section className="book-overview">
       <div className="flex flex-1 flex-col gap-5">
         <h1>{title}</h1>
+        <div className="book-info">
+          <p className="font-semibold text-light-200">
+            By <span>{author}</span>
+          </p>
+          <p className="font-semibold text-light-200">
+            Category <span>{author}</span>
+          </p>
+        </div>
       </div>
     </section>
   );
