00db381e97e837b4d31b87aa78362ec7b00241c6
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-12 23:34:54 +0400

    Update BookCard.tsx

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
 import React from "react";
+import BookCover from "./BookCover";
 
-const BookCard = () => {
-  return <div>BookCard</div>;
+const BookCard = ({
+  id,
+  title,
+  genre,
+  coverColor,
+  coverImage,
+  isLoanedBook = false,
+}: Book) => {
+  return (
+    <li>
+      <Link href={`/books/${id}`}>
+        <BookCover coverColor={coverColor} coverImage={coverImage} />
+      </Link>
+    </li>
+  );
 };
 
 export default BookCard;
