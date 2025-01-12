e515541e2d64454b504141dfcad38cd5415a006d
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-12 22:33:21 +0400

    Update BookCover.tsx

--- components/BookCover.tsx
+++ components/BookCover.tsx
@@ -1,6 +1,28 @@
 import React from "react";
 
-const BookCover = () => {
+type BookCoverVariant = "extraSmall" | "small" | "medium" | "regular" | "wide";
+
+const variantStyles: Record<BookCoverVariant, string> = {
+  extraSmall: "book-cover_extra_small",
+  small: "book-cover_small",
+  medium: "book-cover_medium",
+  regular: "book-cover_regular",
+  wide: "book-cover_wide",
+};
+
+interface Props {
+  className?: string;
+  variant?: BookCoverVariant;
+  coverColor: string;
+  coverUrl: string;
+}
+
+const BookCover = ({
+  className,
+  variant = "regular",
+  coverColor,
+  coverUrl,
+}: Props) => {
   return <div>BookCover</div>;
 };
 
