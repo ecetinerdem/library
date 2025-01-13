c04926d03de972c68ed6dafec3090b830b37f804
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-13 22:00:23 +0400

    Update layout.tsx

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
 import React from "react";
 
 const layout = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <main className="auth-container">
+      <section className="auth-form">
+        <div className="auth-box">
+          <div className="flex flex-row gap-3">
+            <Image src="/icons/logo.svg" alt="logo" width={37} height={37} />
+            <h1 className="text-2xl font-semibold text-white">BookWise</h1>
+          </div>
+          <div>{children}</div>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default layout;
