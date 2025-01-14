94732470c2c756e590fd7bc737c2befa3222f988
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-14 20:25:13 +0400

    add imagekit

--- components/ImageUploader.tsx
+++ components/ImageUploader.tsx
@@ -1,4 +1,15 @@
+"use client";
+
 import React from "react";
+import { IKImage, ImageKitProvider, IKUpload } from "imagekitio-next";
+
+const authenticator = async () => {
+  try {
+    const response = await fetch(`${config.env.apiEndpoint}/api/auth/imagekit`);
+  } catch (error: any) {
+    throw new Error(`Authentication request failed: ${error.message}`);
+  }
+};
 
 const ImageUploader = () => {
   return <div>ImageUploader</div>;
