ddb57d6059c0485518efe80e8bc110cfefcad95e
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-15 21:17:11 +0400

    Update page.tsx

--- app/(root)/my-profile/page.tsx
+++ app/(root)/my-profile/page.tsx
@@ -1,7 +1,24 @@
+import { sampleBooks } from "@/app/constant";
+import { signOut } from "@/auth";
+import BookList from "@/components/BookList";
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+        className="mb-10"
+      >
+        <Button>Logout</Button>
+      </form>
+      <BookList title="Borrowed Books" books={sampleBooks} />
+    </>
+  );
 };
 
 export default page;
