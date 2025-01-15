94d9ca97145c359f3966f20d5752010d1cae2e9b
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-15 19:56:53 +0400

    Update AuthForm.tsx

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -27,6 +27,7 @@ import { Input } from "@/components/ui/input";
 import Link from "next/link";
 import { FIELD_NAMES, FIELD_TYPES } from "@/app/constant";
 import ImageUploader from "./ImageUploader";
+import { toast } from "@/hooks/use-toast";
 
 interface Props<T extends FieldValues> {
   schema: ZodType<T>;
@@ -50,7 +51,17 @@ const AuthForm = <T extends FieldValues>({
   });
 
   // 2. Define a submit handler.
-  const handleSubmit: SubmitHandler<T> = async (data) => {};
+  const handleSubmit: SubmitHandler<T> = async (data) => {
+    const result = await onSubmit(data);
+    if (result.success) {
+      toast({
+        title: "Success",
+        description: isSignIn
+          ? "You have successfully signed in."
+          : "You have successfully signed up",
+      });
+    }
+  };
 
   return (
     <div className="flex flex-col gap-4">
