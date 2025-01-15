31fed0b909f1248211261ca03ef99c638fb7ea78
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-15 15:55:45 +0400

    Update auth.ts

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -1,3 +1,35 @@
 "use server";
 
-const signUp = async (params: AuthCredentials) => {};
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import { eq } from "drizzle-orm";
+import { hash } from "bcryptjs";
+
+const signUp = async (params: AuthCredentials) => {
+    const { fullName, email, password, universityId, universityCard } = params;
+
+    const existingUser = await db
+        .select()
+        .from(users)
+        .where(eq(users.email, email))
+        .limit(1);
+
+        if(existingUser.length > 0) {
+            return {success: false, message: "User already exists"};
+        }
+        const hashedPassword = await hash(password, 10);
+
+        try {
+            await db.insert(users).values({
+                fullName,
+                email,
+                password: hashedPassword,
+                universityId,
+                universityCard
+            })
+
+        } catch (error) {
+            console.log(error, "Signup error");
+            return { success: false, message: "Signup error" };
+        }
+};
