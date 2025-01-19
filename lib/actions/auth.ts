007641591d06b2af99f48119111304fb481f70e4
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-19 21:29:54 +0400

    Update auth.ts

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -5,6 +5,10 @@ import { users } from "@/database/schema";
 import { eq } from "drizzle-orm";
 import { hash } from "bcryptjs";
 import { signIn } from "@/auth";
+import { headers } from "next/headers";
+import ratelimit from "@/lib/ratelimit";
+import { redirect } from "next/navigation";
+
 
 
 export const signInWithCredentials = async (params: Pick<AuthCredentials, "email" | "password">) => {
@@ -27,6 +31,12 @@ export const signInWithCredentials = async (params: Pick<AuthCredentials, "email
   };
 export const signUp = async (params: AuthCredentials) => {
     const { fullName, email, password, universityId, universityCard } = params;
+
+    const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
+
+    const { success } = await ratelimit.limit(ip);
+
+    if (!success) return redirect("/too-fast");
 
     const existingUser = await db
         .select()
